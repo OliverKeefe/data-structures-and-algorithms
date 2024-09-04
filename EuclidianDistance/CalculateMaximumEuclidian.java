2462ac4cc13fbb8f2620d0715bb9a3658dbf4011
OliverKeefe <oliverkeefe@tutanota.com>
2024-09-04 09:03:33 +0100

    Added start of Max Euclidian Distance

---
+++ EuclidianDistance/CalculateMaximumEuclidian.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int robotSim(int[] commands, int[][] obstacles) {
+
+        int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        int[] coords;
+        for (command : commands) {
+            if (command )
+        }
+        
+        // Store the current pos of the robit
+        // 
+
+
+
+    }
+}
